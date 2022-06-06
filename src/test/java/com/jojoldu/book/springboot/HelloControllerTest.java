e051fac9f6161cf042f95837b7f31dfe1929fde0
ingikim <sebilloy@naver.com>
2022-06-06 17:31:30 +0900

    Spring 컨트롤러 동작확인 및 UT 추가

---
+++ src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
@@ -0,0 +1,36 @@
+package com.jojoldu.book.springboot;
+
+import com.jojoldu.book.springboot.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc; // 스프링 MVC 테스트 시작점, HTTP GET, POST등에 대한 API 테스트 가능
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        // get : MockMvc를 통해 /hello 주소로 HTTP GET 요청을 함. 체이닝 지원되어 여러 검증을 이어서 선언 가능
+        // andExpect isok : 결과 검증. HTTP Header의 Status를 검증 (200, 404, 500등.) ok(200)인지 확인
+        // string(hello) : mvc.perform의 결과 검증, 본문 내용 검증, Controller에서 "hello"리턴하는 지
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+// @RunWith
+// 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킴
+// 여기서는 SpringRunner라는 스프링 실행자를 사용
+// 즉 스프링부트 테스트와 JUnit사이에 연결자 역할을 합니다
+}
