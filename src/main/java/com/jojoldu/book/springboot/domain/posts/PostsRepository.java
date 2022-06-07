d0852fb955e126f736c83fd23d7caa10c2e74ac7
ingikim <sebilloy@naver.com>
2022-06-07 23:45:21 +0900

    머스테치로 화면 구성하기-전체조회 화면 만들기

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,8 +1,13 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     // Entity 클래스와 Repository 함께 위치.
     // 기본적인 CRUD메소드 자동 생성됨
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
