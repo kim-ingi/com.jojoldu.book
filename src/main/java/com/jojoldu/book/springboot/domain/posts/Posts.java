a3041b3bcb33f2dfa32905c3791a7d353981693a
ingikim <sebilloy@naver.com>
2022-06-07 17:24:08 +0900

    수정 API 및 테스트 추가

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -39,6 +39,15 @@ public class Posts {
         return this.id;
     }
 
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
     // @Entity
     // 테이블과 링크 될 클래스임을 나타냅니다
     // 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭합니다
