0292b27e9c7d70842a2a97f5af30cdc105c77da2
ingikim <sebilloy@naver.com>
2022-06-08 18:17:13 +0900

    머스테치로 화면 구성하기-삭제기능

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -9,9 +9,9 @@ var main = {
             _this.update();
         });
 
-//        $('#btn-delete').on('click', function () {
-//            _this.delete();
-//        });
+        $('#btn-delete').on('click', function () {
+            _this.del();
+        });
     },
     save : function () {
         var data = {
@@ -49,6 +49,21 @@ var main = {
              data: JSON.stringify(data)
          }).done(function() {
              alert('글이 수정되었습니다.');
+             window.location.href = '/';
+         }).fail(function (error) {
+             alert(JSON.stringify(error));
+         });
+    },
+    del : function () {
+         var id = $('#id').val();
+
+         $.ajax({
+             type: 'DELETE',
+             url: '/api/v1/posts/'+id,
+             dataType: 'json',
+             contentType:'application/json; charset=utf-8'
+         }).done(function() {
+             alert('글이 삭제되었습니다.');
              window.location.href = '/';
          }).fail(function (error) {
              alert(JSON.stringify(error));
